712ac1527757edebf90c8a5488a05376e04d9daf
Jasir <jazyrcp@gmail.com>
2022-10-18 14:11:30 +0530

    created a string util method to convet name

---
+++ src/utils/stringUtils.js
@@ -0,0 +1,41 @@
+export const convertName = name => {
+  let firstLetter = name.charAt(0);
+
+  if (firstLetter <= '9' && firstLetter >= '0') {
+    switch (firstLetter) {
+      case '1':
+        firstLetter = 'One';
+        break;
+      case '2':
+        firstLetter = 'Two';
+        break;
+      case '3':
+        firstLetter = 'Three';
+        break;
+      case '4':
+        firstLetter = 'Four';
+        break;
+      case '5':
+        firstLetter = 'Five';
+        break;
+      case '6':
+        firstLetter = 'Six';
+        break;
+      case '7':
+        firstLetter = 'Seven';
+        break;
+      case '8':
+        firstLetter = 'Eight';
+        break;
+      case '9':
+        firstLetter = 'Nine';
+        break;
+
+      default:
+        break;
+    }
+  } else {
+    firstLetter.toUpperCase();
+  }
+  return firstLetter + name.slice(1);
+};
